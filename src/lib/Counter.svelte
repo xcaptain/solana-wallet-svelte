5172409d37731736bef86321d767a00acc9e7263
Joey <joey.xf@gmail.com>
2024-09-13 12:09:19 +0800

    upgrade to svelte5

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-  let count: number = 0
-  const increment = () => {
-    count += 1
-  }
+  let count: number = $state(0);
 </script>
 
-<button on:click={increment}>
+<button onclick={() => count++}>
   count is {count}
 </button>
